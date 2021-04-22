2d0e39bacf6cd3c05168fc8662153b22d34328b0
Ramin <Ramin.rezaei77@gmail.com>
2021-04-22 17:46:14 +0430

    fix: Abolfazl Bug Report

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,12 +1,15 @@
 // init
+const APP_VERSION = '1.1.4'
 function init(){
     const LOGO = `> Ramin Terminal - (c) 2021 [ raminrezaei.ir ]`
-    let seen = localStorage.getItem('terminal-version')
+    let version = localStorage.getItem('terminal-version')
     insertCommand(LOGO, 'text-command command--pre')
-    if(!seen){
-        insertCommand('type your first command ...', 'warning')
-        localStorage.setItem('terminal-version', '1.1.0')
+    if(version !== APP_VERSION){
         input.innerHTML = 'help'
+        COMMAND_HISTORY = []
+        insertCommand('type your first command ...', 'warning')
+        localStorage.setItem('terminal-version', APP_VERSION)
+        localStorage.setItem('command-history', JSON.stringify([]))
     }
 }
 init()
