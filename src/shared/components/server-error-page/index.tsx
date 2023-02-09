8f1219290fb66b08518bbbd43b668c7ad089fff5
Ramin <r.rezaie@digikala.com>
2023-02-10 01:11:32 +0330

    feat: add server and 404 error

--- src/shared/components/server-error-page/index.tsx
+++ src/shared/components/server-error-page/index.tsx
@@ -1,8 +1,12 @@
+import { LottieAnimation } from '@/shared/components/lottie-animation';
+import SERVER_ERROR_ANIMATION from '@/shared/static/animations/server-error.json';
+
 export function ServerError({ onTryAgain = () => {} }: { onTryAgain: GVoidFunction }) {
   return (
-    <div>
-      <h3>Server Error</h3>
-      <button onClick={onTryAgain}>Try Again ?</button>
+    <div className='w-full select-none h-screen flex items-center justify-center flex-col'>
+      <LottieAnimation data={SERVER_ERROR_ANIMATION} clickable={false} />
+      <h3 className='leading-10 mb-4 text-xl font-bold'>500 - Server Error</h3>
+      <button onClick={onTryAgain}>[ Try Again ]</button>
     </div>
   );
 }
