89da6c7afe4ad6fc9f06c6d66323d38910254798
Ramin <r.rezaie@digikala.com>
2023-07-04 15:27:21 +0330

    feat: new script tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -103,6 +103,24 @@ export default function App({ Component, pageProps }: AppProps) {
             `
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K5SZXFS');`
+            }}
+          />
+
+          <noscript>
+            <iframe
+              src='https://www.googletagmanager.com/ns.html?id=GTM-K5SZXFS'
+              height='0'
+              width='0'
+              className='hidden opacity-0'
+            ></iframe>
+          </noscript>
         </Head>
         <NextNprogress
           height={2}
