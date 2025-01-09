6fdcedfd2c728ad139aeed7858f3c76a4e540629
Ramin <Ramin.rezaei77@gmail.com>
2025-01-09 21:26:46 +0100

    feat: add about me data

--- src/domains/journey/index.tsx
+++ src/domains/journey/index.tsx
@@ -15,7 +15,7 @@ export function JourneyPage() {
           animator({ name: 'fadeInUp' })
         )}
       >
-        The Adventure Started ...
+        {JOURNEY_DATA.title}
       </h1>
       <h3
         className={clsx(
@@ -24,12 +24,11 @@ export function JourneyPage() {
           animator({ name: 'fadeIn', delay: '1s' })
         )}
       >
-        On a journey to <span className='text-2xl font-bold'>Google</span>, driven by
-        curiosity and a desire to learn more.
+        {JOURNEY_DATA.description}
       </h3>
 
       <div className='mt-20 flex flex-col gap-28'>
-        {JOURNEY_DATA.map((item: JourneyItem, index: number) => (
+        {JOURNEY_DATA.items.map((item: JourneyItem, index: number) => (
           <JourneyCard key={index} order={index + 1} data={item} />
         ))}
       </div>
@@ -40,7 +39,7 @@ export function JourneyPage() {
           animator({ name: 'fadeIn' })
         )}
       >
-        Continuing my journey to Google...
+        {JOURNEY_DATA.footer}
       </p>
     </ContentContainer>
   );
