d213310868fb0282cd706f554fd74f418b838de8
Ramin <39030920+raminr77@users.noreply.github.com>
2025-02-07 10:24:13 +0100

    Update recommendations.ts

--- src/data/recommendations.ts
+++ src/data/recommendations.ts
@@ -6,13 +6,33 @@ export interface RecommendationItem {
   title: string;
   image: string;
   caption: string;
-  fullname: string;
+  fullName: string;
 }
 
 export const RECOMMENDATIONS: RecommendationItem[] = [
   {
+    id: 15,
+    fullName: 'Igor Karanović',
+    title: 'Team Lead | Senior Back-End Developer at Boozt',
+    text: "I had the pleasure of working closely with Ramin on our recent CMS project. He was an invaluable asset to our team. Ramin's quick learning curve, coupled with his dedication, made him a standout. His ability to grasp complex concepts and implement them efficiently was truly impressive. We successfully launched our Headless CMS within a year, with Ramin having an important role in the process. Without a doubt, Ramin is a talented and motivated individual who would be an asset to any team. I highly recommend him.",
+    date: 'February 7, 2025',
+    caption: 'Igor managed Ramin directly',
+    url: 'https://www.linkedin.com/in/igorkaranovic/',
+    image: 'https://media.licdn.com/dms/image/v2/C4E03AQFelIsCu8NTEw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1604431517435?e=1744243200&v=beta&t=YjqXCeVbb2Ol0z-5WON4sfaFWMxvwMsR6LuNWWFsOZk'
+  },
+  {
+    id: 14,
+    fullName: 'Dimitar Pashovski',
+    title: 'Senior frontend developer at Boozt',
+    text: 'Ramin is a talented frontend developer with strong React skills and a great work ethic. He consistently delivers high-quality work and is always willing to collaborate and share his knowledge. His problem-solving skills and attention to detail make him a valuable asset to any team. I highly recommend him!',
+    date: 'February 7, 2025',
+    caption: 'Dimitar worked with Ramin in the same team',
+    url: 'https://www.linkedin.com/in/pashata',
+    image: 'https://media.licdn.com/dms/image/v2/C4D03AQH__mOhducx2A/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1630859707917?e=1744243200&v=beta&t=LI0vIcER-JF_4X8g8Ki0Z8ne0i3OGDJ5D301wieBoHk'
+  },
+  {
     id: 1,
-    fullname: 'Erfan Khavarian',
+    fullName: 'Erfan Khavarian',
     title: 'Team Lead | Senior Back-End Developer at Boozt',
     text: 'I had the pleasure of working with Ramin on a project where he joined our team for a short period. As a frontend developer, he quickly adapted to our workflow and played a key role in ensuring we delivered the project on time. Ramin is hardworking, fast, and highly knowledgeable. He’s always eager to learn new things and never backs down from a challenge. His ability to grasp new concepts and apply them effectively makes him a great asset to any development team.',
     date: 'February 1, 2025',
@@ -22,7 +42,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 2,
-    fullname: 'Emil Azizov',
+    fullName: 'Emil Azizov',
     title: 'Backend Developer',
     text: 'I worked with Ramin in the same team for more than 1 year on both the CMS and Promobar projects.  Ramin is a creative and reliable team player who always brings fresh ideas to the table. He\'s not afraid to tackle complex problems and always finds practical solutions.  You can trust Ramin to deliver high-quality work on time. Plus, he\'s always eager to learn new things and pays close attention to details like performance optimization. I\'m really happy I got to work with him.',
     date: 'January 23, 2025',
@@ -32,7 +52,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 3,
-    fullname: 'Andra Norvaiša',
+    fullName: 'Andra Norvaiša',
     title: 'Product Owner at DFDS',
     text: 'I had the pleasure of working with Ramin for a year, and he is a talented developer. Ramin brought creativity, a proactive mindset, and a collaborative approach to our projects, making him a valued team member.',
     date: 'November 21, 2024',
@@ -42,7 +62,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 4,
-    fullname: 'Kaveh Alipour',
+    fullName: 'Kaveh Alipour',
     title: 'Mobile Engineer',
     text: 'I absolutely recommend Ramin! His friendly and responsible approach and fantastic expertise made working together a total joy. Each project became a blast thanks to his positive vibes. Ramin\'s collaborative spirit and knowledge make him an invaluable asset to any team.',
     date: 'January 21, 2024',
@@ -52,7 +72,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 5,
-    fullname: "Faezeh Padidar",
+    fullName: "Faezeh Padidar",
     title: "Quality Assurance Engineer",
     text: "I had have the pleasure of working with Ramin for more than two years at Digikla company. Ramin is one of the best programmers I know. He is productive person with vast knowledge in coding. Ramin is proactive self-motivated and intelligence person. He is a well- connected professional that always takes to time support anyone in his team. Ramin would be a true asset for any position.",
     date: "August 13, 2022",
@@ -62,7 +82,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 6,
-    fullname: "Sana Mohammadzadeh",
+    fullName: "Sana Mohammadzadeh",
     title: "Product Manager at Exploria | Former Digikala PM",
     text: "When it comes to work, Ramin is someone you can rely on.  He will always be available to help you and answer any question or concern you may have with good manners. He is passionate about his job, and he keeps all deadlines on time. It is a pleasure to work with Ramin.",
     date: "July 25, 2022",
@@ -72,7 +92,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 7,
-    fullname: "Arash Saadatmand",
+    fullName: "Arash Saadatmand",
     title: "Product Manager",
     text: "In the engineering world, I have learned, that finding someone you can rely on is precious. Knowing that projects won't be let down and there is someone you can call out to when needed, with high standards and high-quality work, is something that no one is willing to lose. I have, and am enjoying working with Ramin and hope it will last.",
     date: "July 23, 2022",
@@ -82,7 +102,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 8,
-    fullname: "Hadi Omidi",
+    fullName: "Hadi Omidi",
     title: "Software Engineer | Game Theory Enthusiast",
     text: "I don't want to compliment Ramin because everyone knows his excellent skills. One of Ramin's most important features is his fixation on his company and his work. His contributions were precious to the community. Furthermore, his work ethic is flawless, and it is so easy to work together. He goes out of his way when you ask for some help and guidance. I highly recommend Ramin and would love to work together again.",
     date: "April 25, 2022",
@@ -92,7 +112,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 9,
-    fullname: "Ross Amiri",
+    fullName: "Ross Amiri",
     title: "Software Engineer | Team-lead at TrustYou",
     text: "Let's add some great features to the world! just make a couple more of Ramins, and the job is already done! Passionate, responsible and always trying his best to give the best solutions to the problems. Self-learner and strongly motivated on anything related to computers. A lovely must-have friend.",
     date: "January 16, 2022",
@@ -102,7 +122,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 10,
-    fullname: "Sepideh Gheisar",
+    fullName: "Sepideh Gheisar",
     title: "Senior Product Manager",
     text: "I have worked with many professionals throughout my work, but Ramin was a unique one on them. His ability to go out of his way to help others has made him stand out. I can say that Ramin has two very prominent characteristics and that speed of action in finding useful solutions and having a very good work ethic that makes each member of the product team want to work with Ramin. His expertise as a developer is considerable, and it helped our team come up with more efficient solutions on different projects. In addition his contribution is valuable to the side, and I highly recommend Ramin and would love to work with him again :)",
     date: "October 16, 2021",
@@ -112,7 +132,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 11,
-    fullname: "Alireza Khosravian",
+    fullName: "Alireza Khosravian",
     title: "Software Engineer at textkernel",
     text: "There is no better colleague than Ramin. He is one of the most dedicated professionals I’ve worked with and is willing to put that extra help whenever you need it. His expertise as a front-end developer is considerable. He knows how to work with TAGS like a God when decides to create. I highly recommend Ramin and would love to work with him again. :D",
     date: "August 13, 2021",
@@ -122,7 +142,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 12,
-    fullname: "Amirreza Balouchi",
+    fullName: "Amirreza Balouchi",
     title: "Experienced Software Engineer | Machine Learning Enthusiast | Data Science & AI Solutions | DevOps",
     text: "He does what he says and his work is always top-notch. Hardworking, intelligent, and helpful, he has come a long way and is sure to achieve greater heights.",
     date: "May 11, 2021",
@@ -132,7 +152,7 @@ export const RECOMMENDATIONS: RecommendationItem[] = [
   },
   {
     id: 13,
-    fullname: "Alireza Khalili",
+    fullName: "Alireza Khalili",
     title: "Senior DevOps Engineer at CHECK24 GmbH",
     text: "A great programmer who is highly skilled, great team player, motivated and funny guy. it was a pleasure working with him.",
     date: "October 10, 2020",
