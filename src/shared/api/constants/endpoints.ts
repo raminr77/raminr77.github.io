875a75bc8cb4d29a9e2dbf005d439e9b3484b608
Ramin Rezaei <rare@boozt.com>
2025-02-19 00:08:18 +0100

    fix: some ui issues

--- src/shared/api/constants/endpoints.ts
+++ src/shared/api/constants/endpoints.ts
@@ -1,10 +1,5 @@
 export const CONTACT_ME_ENDPOINTS = {
   sendMessage: 'https://email-api.ramiin.workers.dev/',
-  sendTextToAI: ({
-    text,
-    userId,
-  }: {
-    text: string;
-    userId: string;
-  }) => `/api/ai/?text=${text}&userId=${userId}`
+  sendTextToAI: ({ text, userId }: { text: string; userId: string }) =>
+    `/api/ai/?text=${text}&userId=${userId}`
 } as const;
