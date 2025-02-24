224fa71a364584b5356b8d574af86b0621c0e299
Ramin Rezaei <rare@boozt.com>
2025-02-24 19:48:03 +0100

    feat: add post filters

--- src/domains/posts/helpers/posts.ts
+++ src/domains/posts/helpers/posts.ts
@@ -7,6 +7,21 @@ export function postSorter(first: Post, second: Post) {
   return compareDesc(new Date(first.date), new Date(second.date));
 }
 
+export function filterPostsByKey(
+  postItem: Post,
+  filter: Record<'tag' | 'category', string> | null = null
+) {
+  if (!filter) {
+    return true;
+  }
+
+  if (filter.tag) {
+    return postItem.tags.includes(filter.tag);
+  }
+
+  return postItem.category === filter.category;
+}
+
 export function readingTime(text: string) {
   const wpm = 300;
   const words: number = text.trim().split(/\s+/).length;
--- src/domains/posts/post-list-page.tsx
+++ src/domains/posts/post-list-page.tsx
@@ -1,21 +1,30 @@
 import type { Metadata } from 'next';
-
+import Link from 'next/link';
 import { clsx } from 'clsx';
 
 import { ContentContainer } from '@/layout/components/content-container';
+import { filterPostsByKey, postSorter } from '@/domains/posts/helpers';
 import { PostCard } from '@/domains/posts/components/post-card';
 import { allPosts, type Post } from 'contentlayer/generated';
-import { postSorter } from '@/domains/posts/helpers';
+import { ROUTES } from '@/shared/constants';
 import { animator } from '@/shared/helpers';
 import { titleFont } from '@/app/fonts';
 import { PERSONAL_DATA } from '@/data';
+
+interface PostListPageProps {
+  searchParams: Record<'tag' | 'category', string>;
+}
 
 export const metadata: Metadata = {
   title: 'Posts'
 };
 
-export async function PostListPage() {
-  const posts: Post[] = allPosts.sort(postSorter);
+export async function PostListPage({ searchParams }: PostListPageProps) {
+  const filter = Object.keys(searchParams).length > 0 ? searchParams : null;
+  const posts: Post[] = allPosts
+    .filter((postItem: Post) => filterPostsByKey(postItem, filter))
+    .sort(postSorter);
+
   return (
     <ContentContainer animationName="fadeIn">
       <h1
@@ -27,6 +36,24 @@ export async function PostListPage() {
       >
         {`${PERSONAL_DATA.firstName}'s Post`}
       </h1>
+
+      {posts.length === 0 && (
+        <div
+          className={clsx(
+            'flex items-center flex-col gap-2 justify-center w-full mt-10',
+            animator({ name: 'bounceInUp' })
+          )}
+        >
+          <p className={clsx('text-center text-xl font-bold', titleFont.className)}>
+            No posts found.
+          </p>
+          <p>There are no posts that match your search or filters.</p>
+          <Link className="text-amber-500 mt-3" href={ROUTES.POSTS}>
+            Clear Your Filter
+          </Link>
+        </div>
+      )}
+
       <div className="mt-8 grid grid-cols-3 gap-4 overflow-hidden max-lg:grid-cols-2 max-md:grid-cols-1">
         {posts.map((post: Post, index: number) => (
           <PostCard key={post.id} data={post} animationDelay={(index + 1) * 0.3} />
