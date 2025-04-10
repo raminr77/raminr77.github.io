2395142e55849636c109483b818fbf86866bb995
Ramin Rezaei <rare@boozt.com>
2025-04-10 14:03:34 +0200

    feat: change cache logic

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,34 @@
-const CACHE_NAME = 'ramin-rezaei-cache';
+const CACHE_NAME = 'static-cache-v1';
+const STATIC_FILES_URL_PATTERN = /\/images\/.*\.(png|jpg|jpeg|gif|webp|svg)$/;
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['/']);
-    })
-  );
+self.addEventListener("install", () => {
+  console.log("Service Worker installed.");
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated.");
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  const requestUrl = new URL(event.request.url);
+
+  if (STATIC_FILES_URL_PATTERN.test(requestUrl.pathname)) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+          return fetch(event.request).then((networkResponse) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      })
+    );
+  } else {
+    event.respondWith(fetch(event.request));
+  }
 });
