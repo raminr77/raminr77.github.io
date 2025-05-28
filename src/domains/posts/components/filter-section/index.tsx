3c3010f6f0145fa25b053e4e1e34f9f03ed6e4e5
Ramin <Ramin.rezaei77@gmail.com>
2025-05-29 01:59:06 +0200

    feat: change clear filter button style

--- src/domains/posts/components/filter-section/index.tsx
+++ src/domains/posts/components/filter-section/index.tsx
@@ -7,19 +7,24 @@ interface FilterSectionProps {
   hasActiveFilter: boolean;
 }
 
-const BUTTON_CLASSES = "border px-4 py-1 text-md hover:border-amber-500 bg-transparent shadow backdrop-blur-sm duration-500 hover:bg-slate-300/5";
-
 export function FilterSection({ categories = [], hasActiveFilter = false }: FilterSectionProps) {
   return (
     <div className="flex items-center flex-wrap gap-3">
       {categories.map((item) => (
-        <Link key={item} href={`?category=${item}`} className={BUTTON_CLASSES}>
+        <Link
+          key={item}
+          href={`?category=${item}`} 
+          className="border px-4 py-1 text-md hover:border-amber-500 bg-transparent shadow backdrop-blur-sm duration-500 hover:bg-slate-300/5"
+        >
           {item}
         </Link>
       ))}
 
       {hasActiveFilter && (
-        <Link href={ROUTES.POSTS} className={BUTTON_CLASSES}>
+        <Link
+          href={ROUTES.POSTS}
+          className="border px-4 py-1 text-md border-amber-500 bg-transparent shadow backdrop-blur-sm bg-slate-300/5"
+        >
           Clear Filter
         </Link>
       )}
