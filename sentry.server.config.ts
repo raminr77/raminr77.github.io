7bf27f3b220136a6a2720e066f0027cdae7a7c80
Ramin <ramin.rezaei77@gmail.com>
2025-08-13 12:47:42 +0200

    feat: disable sentry for local

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: 'https://69306522276668f196ff4957161faa4c@o4508781012582400.ingest.de.sentry.io/4508781018939472',
-  tracesSampleRate: 1,
-  debug: false
-});
+if (process.env.SENTRY_ENABLED === "true") {
+  Sentry.init({
+    dsn: process.env.SENTRY_URL,
+    tracesSampleRate: 1,
+    debug: false
+  });
+}
