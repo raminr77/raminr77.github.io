2f8ec5b4217ba882ba40d811e96e43d1f4d8a661
Ramin <Ramin.rezaei77@gmail.com>
2025-10-05 01:55:48 +0200

    fix: toast issue

--- src/shared/services/email-service.ts
+++ src/shared/services/email-service.ts
@@ -34,11 +34,12 @@ export const sendEmail = (data: requestData) => {
               message: response.message || 'Your message has been sent successfully.'
             });
             resolve(response);
+          } else {
+            notify.error({
+              message: response.message || 'We could not send your message now!'
+            });
+            reject();
           }
-          notify.error({
-            message: response.message || 'We could not send your message now!'
-          });
-          reject();
         })
         .catch(() => {
           notify.error({ message: 'We could not send your message now!' });
