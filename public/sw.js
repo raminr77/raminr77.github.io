42523f947e7a5473a663ef70cf0d272fd9db7693
Ramin <ramin.rezaei77@gmail.com>
2025-10-29 12:25:52 +0100

    fix: sw issues and 404 errors

--- public/sw.js
+++ public/sw.js
@@ -14,6 +14,10 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   const requestUrl = new URL(event.request.url);
 
+  if (url.searchParams.has('_rsc')) {
+    return;
+  }
+
   if (STATIC_FILES_URL_PATTERN.test(requestUrl.pathname)) {
     event.respondWith(
       caches.open(CACHE_NAME).then((cache) => {
