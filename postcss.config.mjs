6729677c7eec84d1bb75c8cb363ff253e2845cec
Ramin <ramin.rezaei77@gmail.com>
2025-11-05 14:10:05 +0100

    feat: Add test configs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}
-  }
+    '@tailwindcss/postcss': {},
+    '@fullhuman/postcss-purgecss': {
+      content: [
+        './src/**/*.{js,jsx,ts,tsx}',
+      ],
+      safelist: ['body', 'html', /^animate__/],
+    },
+  },
 };
 
 export default config;
