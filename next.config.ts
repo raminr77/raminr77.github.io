f9a79d80779c6b7ff0746e69267b6e12e4daa7bb
Ramin <ramin.rezaei77@gmail.com>
2025-11-06 12:31:48 +0100

    fix: formatting

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true'
 });
 
 const config: NextConfig = {
