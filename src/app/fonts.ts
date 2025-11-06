99f80bf9c78b1c2c634d766287e8c05b0ca8cc14
Ramin <ramin.rezaei77@gmail.com>
2025-11-06 11:50:36 +0100

    feat: performance improvments

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,7 +3,8 @@ import { Gantari, Hubballi } from 'next/font/google';
 export const textFont = Hubballi({
   preload: true,
   weight: '400',
-  display: 'swap'
+  display: 'swap',
+  subsets: ['latin']
 });
 
 export const titleFont = Gantari({
