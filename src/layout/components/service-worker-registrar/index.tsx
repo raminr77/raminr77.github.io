1ac955a94f18e1a6c4b5ef4257a44bcd8c899408
Ramin <ramin.rezaei77@gmail.com>
2026-02-24 15:13:45 +0100

    fix: small improvment

--- src/layout/components/service-worker-registrar/index.tsx
+++ src/layout/components/service-worker-registrar/index.tsx
@@ -4,9 +4,10 @@ import { useEffect } from 'react';
 export function ServiceWorkerRegistrar() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js').catch(() => {
-        console.log('Service Worker registration successful.');
-      });
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(() => console.log('Service Worker registered.'))
+        .catch((error) => console.log('Service Worker registration failed.', error));
     }
   }, []);
   return null;
